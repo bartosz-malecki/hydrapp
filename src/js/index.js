8aa0e0d00e1402270a3131c05049fa8ae03e0bbd
Bartosz Małecki <bartoszmalecki90@gmail.com>
2020-10-08 22:35:59 +0200

    js trying

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,31 @@ registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const glassCounter = document.querySelector('.glass__counter--js');
+const buttonAdd = document.querySelector('.button__add--js');
+const buttonRemove = document.querySelector('.button__remove--js');
+const key = new date().toISOString().slice(0, 10);
+
+let currentGlassCounter = 0;
+
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue) {
+    currentGlassCounter = localStorageValue;
+} else {
+    localStorage.setItem(key, 0);
+}
+
+
+
+buttonAdd.addEventListener('click', () => {
+    currentGlassCounter++;
+    glassCounter.innerHTML = currentGlassCounter;
+})
+
+buttonRemove.addEventListener('click', () => {
+    if (currentGlassCounter > 0) {
+        currentGlassCounter--;   
+    }
+    glassCounter.innerHTML = currentGlassCounter;
+})
