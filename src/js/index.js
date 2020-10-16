de6a659ced9ff6611a556b6c54d0935330b819cb
Bartosz Malecki <bartoszmalecki90@gmail.com>
2020-10-16 13:49:51 +0200

    cleaned folder

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,15 @@
-import { data } from 'autoprefixer';
-import '../scss/main.scss';
+import { data } from "autoprefixer";
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-import {registerSW} from './pwa.js';
+import { registerSW } from "./pwa.js";
 registerSW();
 
 /* place your code below */
 
-const glassCounter = document.querySelector('.glass__counter--js');
-const buttonAdd = document.querySelector('.button__add--js');
-const buttonRemove = document.querySelector('.button__remove--js');
+const glassCounter = document.querySelector(".glass__counter--js");
+const buttonAdd = document.querySelector(".button__add--js");
+const buttonRemove = document.querySelector(".button__remove--js");
 const key = new Date().toISOString().slice(0, 10);
 
 let currentGlassCounter = 0;
@@ -17,38 +17,34 @@ let currentGlassCounter = 0;
 const localStorageValue = localStorage.getItem(key);
 
 if (localStorageValue) {
-    currentGlassCounter = localStorageValue;
+  currentGlassCounter = localStorageValue;
 } else {
-    localStorage.setItem(key, 0);
+  localStorage.setItem(key, 0);
 }
 
 glassCounter.innerHTML = currentGlassCounter;
 
+buttonAdd.addEventListener("click", () => {
+  currentGlassCounter++;
+  glassCounter.innerHTML = currentGlassCounter;
+  localStorage.setItem(key, currentGlassCounter);
+});
 
-buttonAdd.addEventListener('click', () => {
-    currentGlassCounter++;
-    glassCounter.innerHTML = currentGlassCounter;
+buttonRemove.addEventListener("click", () => {
+  if (currentGlassCounter > 0) {
+    currentGlassCounter--;
     localStorage.setItem(key, currentGlassCounter);
-})
-
-buttonRemove.addEventListener('click', () => {
-    if (currentGlassCounter > 0) {
-        currentGlassCounter--;
-        localStorage.setItem(key, currentGlassCounter);   
-    }
-    glassCounter.innerHTML = currentGlassCounter;
-})
+  }
+  glassCounter.innerHTML = currentGlassCounter;
+});
 
 let removeClass = true;
-const changeButton = document.querySelector('.button__add--js');
+const changeButton = document.querySelector(".button__add--js");
 
-changeButton.addEventListener('click', () => {
-    
-    if (removeClass) {
-        const newAddButton = document.querySelector('.button__add--js');
-        newAddButton.classList.remove('button__add--js');
-        removeClass = false;
-    }
-})
-
-
+changeButton.addEventListener("click", () => {
+  if (removeClass) {
+    const newAddButton = document.querySelector(".button__add--js");
+    newAddButton.classList.remove("button__add--js");
+    removeClass = false;
+  }
+});
